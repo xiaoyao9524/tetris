ce85c74fbd83269db7b0a468342b0cfc9c68297f
xiaoyao <xiaoyao9524@163.com>
2022-02-16 00:13:36 +0800

    修改根路径

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,20 +3,23 @@ import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
+    name: 'Home',
     component: () => import('@/views/Home.vue')
   },
   {
     path: '/about',
+    name: 'About',
     component: () => import('@/views/About')
   },
   {
     path: '/play',
+    name: 'Play',
     component: () => import('@/views/Play')
   },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/tetris/'),
   routes,
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tetris/',
   server: {
     host: '0.0.0.0',
     port: 3000,
