80c15e31a31b6198ae0af012f0c13c0013eafa55
xiaoyao9524 <xiaoyao9524@163.com>
2022-02-16 16:28:40 +0800

    新增环境变量并部署第一版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const { resolve } = require('path')
 
+const config = loadEnv('', './')
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tetris/',
+  base: config.VITE_APP_BASE_URL,
   server: {
     host: '0.0.0.0',
     port: 3000,
